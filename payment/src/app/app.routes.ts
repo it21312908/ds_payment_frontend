fabc48826d811117db8154bdd6f66f6df59fef18
it21312908 <it21312908@my.sliit.lk>
2024-04-25 07:16:31 +0530

    bootstrap added

--- payment/src/app/app.routes.ts
+++ payment/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { PaymentFormComponent } from './payment-form/payment-form.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'payment',
+        component: PaymentFormComponent,
+    },
+];
--- payment/src/app/payment-form/payment-form.component.ts
+++ payment/src/app/payment-form/payment-form.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-payment-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, RouterLink, RouterModule],
   templateUrl: './payment-form.component.html',
   styleUrl: './payment-form.component.css'
 })
 export class PaymentFormComponent {
+
+  paymentForm: FormGroup;
+
+  constructor(){
+    this.paymentForm = new FormGroup({
+      course: new FormControl(""),
+      fee: new FormControl(""),
+      paymentMethod: new FormControl(""),
+      description: new FormControl("")
+    })
+  }
 
 }
